4c37a05f53ee3a18908f617169fafe473e398c6f
Suvigya Saxena <suvigya2001@gmail.com>
2021-05-13 22:28:37 +0530

    Added guidelines to follow

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const ThingSpeak= require('thingspeakclient');
+const ThingSpeak= require('thingspeakclient'); //install thingspeakclient using the command npm i thingspeakclient go to npmpjs.com to get more info over thingspeakclient module
 const app = express();
 var client = new ThingSpeak();
 
-app.use(express.json());
+app.use(express.json()); //Using this can render javascript object from res.send 
 
 var fieldData;
 client.getLastEntryInFieldFeed(12397, 1, (err, resp)=>{
@@ -11,7 +11,8 @@ client.getLastEntryInFieldFeed(12397, 1, (err, resp)=>{
         fieldData = resp;
     }
 });
-
+// We can use the above function to get the last entry in the field of the public channel 
+// Use JSON Viewer PRO extension in chrome to see the json in browser
 app.get('/', (req, res) => {
     res.send(fieldData); 
 });
